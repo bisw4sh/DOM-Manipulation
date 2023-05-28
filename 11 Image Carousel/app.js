d23f61234579b0b6376aa959994b71832aff5aee
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-05-28 20:42:17 +0545

    with setInterval next manual click isnot working

---
+++ 11 Image Carousel/app.js
@@ -0,0 +1,51 @@
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const container = document.querySelector('.container')
+const imagesel = document.querySelector('.imagesel')
+
+const source = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg' ]
+
+prev.addEventListener('click', () => {
+    const src = imagesel.getAttribute("src")
+    let index = Number(src[9])
+
+    if(index == 0){
+    imagesel.classList.add('.fadeinout')
+        imagesel.setAttribute("src", `./images/${source.length - 1}.jpg`)
+    }
+    else{
+    imagesel.classList.add('fadeinout')
+        imagesel.setAttribute("src", `./images/${ index - 1}.jpg`)
+    }
+    imagesel.classList.add('fadeinout')
+    console.log(index)
+
+    setTimeout(() => {
+        imagesel.classList.remove('fadeinout');
+      }, 1000)
+})
+
+const nextPic = () => {
+    const src = imagesel.getAttribute("src")
+    let index = Number(src[9])
+
+    if(index == source.length - 1){
+    imagesel.classList.add('fadeinout')
+        imagesel.setAttribute("src", `./images/0.jpg`)
+    }
+    else{
+    imagesel.classList.add('fadeinout')
+        imagesel.setAttribute("src", `./images/${ index + 1}.jpg`)
+    }
+    console.log(index)
+
+    setTimeout(() => {
+        imagesel.classList.remove('fadeinout');
+      }, 1000)
+}
+
+next.addEventListener('click', nextPic())
+
+setInterval(() => {
+    nextPic()
+}, 5000)

c77b3c22cd10bb72c7bd64dfeddc27ed51a8fab5
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-05-30 12:11:39 +0545

    script to update

---
+++ 15 CountDown to New Year/app.js
@@ -0,0 +1,28 @@
+const days = document.querySelector('.day')
+const hrs = document.querySelector('.hrs')
+const min = document.querySelector('.min')
+const sec = document.querySelector('.sec')
+
+const newYearTime = new Date("Jan 1, 2024 00:00:00").getTime();
+
+updateCountdown();
+
+function updateCountdown() {
+  const now = new Date().getTime();
+  const gap = newYearTime - now;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const d = Math.floor(gap / day);
+  const h = Math.floor((gap % day) / hour);
+  const m = Math.floor((gap % hour) / minute);
+  const s = Math.floor((gap % minute) / second);
+  days.innerText = d;
+  hrs.innerText = h;
+  min.innerText = m;
+  sec.innerText = s;
+  setTimeout(updateCountdown, 1000)
+}

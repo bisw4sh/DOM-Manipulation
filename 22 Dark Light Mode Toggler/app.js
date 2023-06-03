766642e5a2365169e230eb1f620e7e2cd9b2c50b
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-06-03 22:52:19 +0545

    toggle script

---
+++ 22 Dark Light Mode Toggler/app.js
@@ -0,0 +1,7 @@
+const checkbox = document.querySelector('.checkbox')
+const slider = document.querySelector('.checkbox')
+
+
+checkbox.addEventListener('click', ()=> {
+    document.body.classList.toggle('dark')
+})

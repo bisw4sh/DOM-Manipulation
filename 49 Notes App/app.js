68f57e687d6ae1a2f1ec3b022b71f25ebf65323a
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-06-12 17:37:07 +0545

    v1.0 added localStorage

--- 49 Notes App/app.js
+++ 49 Notes App/app.js
@@ -1,10 +1,12 @@
 const add = document.querySelector('.add')
-const edit = document.querySelector('.fa-solid.fa-pen-to-square')
-const notetext = document.querySelector('#notetext')
 const notes = document.querySelector('.notes')
 
+let readOnly = false
+let notesArr = localStorage.getItem('notesArr') ? JSON.parse(localStorage.getItem('notesArr')) : []
 
-add.addEventListener('click', () => {
+if(notesArr){ notesArr.forEach( note => newNoteFxn(note)) }
+
+function newNoteFxn(txt) {
     let newNote = document.createElement('div')
     newNote.classList.add('note')
     const newNoteText = `<header class="note-header">
@@ -12,17 +14,51 @@ add.addEventListener('click', () => {
                             <i class="fa-sharp fa-solid fa-trash"></i>
                         </header> 
                         <div class="textWrapper">
-                            <textarea name="notetext" id="notetext" cols="40" rows="13"></textarea>
+                            <textarea name="notetext" id="notetext" cols="40" rows="13">${typeof(txt) === 'string' ? txt : ''}</textarea>
                         </div>`
-
     newNote.innerHTML = newNoteText
     notes.appendChild(newNote)
-})
+}
+
+add.addEventListener('click', newNoteFxn)
 
 notes.addEventListener('click', (event) => {
-    const target = event.target;
+    const target = event.target
+
+    //Event for delete
     if (target.classList.contains('fa-sharp') && target.classList.contains('fa-solid') && target.classList.contains('fa-trash')) {
-        const note = target.parentElement.parentElement;
-        notes.removeChild(note);
+        const note = target.parentElement.parentElement
+        notes.removeChild(note)
+        updateLS()
     }
-});
+
+    // Event for edit
+    if (target.classList.contains('fa-solid') && target.classList.contains('fa-pen-to-square')) {
+        const note = target.parentElement.parentElement
+        const notetext = note.querySelector('textarea')
+        readOnly = !readOnly
+        notetext.readOnly = readOnly
+        if(readOnly){
+            notetext.style.backgroundColor = '#eaebeb'
+        }
+        else{
+            notetext.style.backgroundColor = 'white'
+        }
+    }
+
+    //For Change in Text Area
+    document.querySelectorAll('#notetext').forEach(
+        singleNoteTxt => {
+            singleNoteTxt.addEventListener('change', () => {
+                updateLS()
+            })
+        }
+    )
+})
+
+function updateLS(){
+    const notesText = document.querySelectorAll('textarea')
+    const notes = []
+    notesText.forEach(note => notes.push(note.value))
+    localStorage.setItem('notesArr', JSON.stringify(notes))
+}
